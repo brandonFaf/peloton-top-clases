1982bd9f376a951987e8258201dc1fa0e023669d
Brandon Myers <brandonstrasn91@yahoo.com>
2020-12-12 14:37:15 -0500

    better login

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,23 +4,32 @@ const Login = ({ setUser }) => {
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
   const [error, setError] = useState();
+  const [loading, setLoading] = useState();
   const handleUsername = e => {
     setUsername(e.target.value);
   };
   const handlePassword = e => {
     setPassword(e.target.value);
   };
-  const login = async () => {
+  const login = async e => {
+    e.preventDefault();
+    setLoading('Loading...');
+    setError('');
     const userId = await authenticate(username, password).catch(e => {
       setError('there was an error logging in');
     });
     if (userId) {
       localStorage.setItem('userId', userId);
       setUser(userId);
+    } else {
+      setError(
+        'Could not find user. Please make sure email and password are correct'
+      );
+      setLoading('');
     }
   };
   return (
-    <div>
+    <form>
       <label htmlFor='username'>Username</label>
       <input
         type='text'
@@ -35,11 +44,12 @@ const Login = ({ setUser }) => {
         onChange={handlePassword}
         value={password}
       />
-      {error && <div>{error}</div>}
-      <button type='button' onClick={login}>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
+      {loading && <div>{loading}</div>}
+      <button type='submit' onClick={login}>
         Submit
       </button>
-    </div>
+    </form>
   );
 };
 
