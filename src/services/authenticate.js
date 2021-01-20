517695944785b5ff9c6539f91bf534ef987ce6ab
Brandon Myers <brandonstrasn91@yahoo.com>
2021-01-19 21:43:21 -0500

    update api url

--- src/services/authenticate.js
+++ src/services/authenticate.js
@@ -1,16 +1,13 @@
 export default async (username, password) => {
-  const res = await fetch(
-    'https://pelotop-server-git-serverless.brmyers2013.vercel.app/api/login',
-    {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username,
-        password
-      }),
-      credentials: 'include'
-    }
-  );
+  const res = await fetch('https://pelotop-server.vercel.app//api/login', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      username,
+      password
+    }),
+    credentials: 'include'
+  });
   const { user_id, error } = await res.json();
   if (error) {
     throw new Error(error);
